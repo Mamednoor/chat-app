bd6aa274e7a1f2080752e05cdddc61c7dc145c85
Mamednoor <mamed.gohabur@gmail.com>
2020-03-31 15:23:39 +0200

    addLink

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -5,31 +5,31 @@ const users = [
 
     {
         name: "Robert Reyes",
-        avatar: "https://randomuser.me/api/portraits/men/28.jpg",
+        avatar: "https://i.skyrock.net/2318/19122318/pics/542003866_small.jpg",
         online: false
     },
     
     {
         name: "Nellie Caldwell",
-        avatar: "https://randomuser.me/api/portraits/women/17.jpg",
+        avatar: "https://i.skyrock.net/1085/13431085/pics/345224551.jpg",
         online: true
     },
     
     {
         name: "Vernon Mason",
-        avatar: "https://randomuser.me/api/portraits/men/84.jpg",
+        avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSor4mgdlMoNibiYxM5S3hbEiagOi3zD3kmoTUjiC3gLUihknCK&usqp=CAU",
         online: true
     },
     
     {
         name: "Erica Hunt",
-        avatar: "https://randomuser.me/api/portraits/women/87.jpg",
+        avatar: "https://www.catizz.com/medias/common/chat-grosse-tete-mini.jpg",
         online: false
     },
     
     {
         name: "Juanita Phillips",
-        avatar: "https://randomuser.me/api/portraits/women/55.jpg",
+        avatar: "https://lh3.googleusercontent.com/proxy/F5mki-SjIwLBv1MVBoYCrSib_th9NskUBUXftcGxyHdtvDhlkkXz4N2-jTolk-HoBiRXuYBl5h1PoVXFNGExkJ6z90ve98X6G6v0nItjY_9CbQ",
         online: true
     }
     ]
