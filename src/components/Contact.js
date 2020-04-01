913276832566a5cf6b6bb1eb83cc2152adf65c76
Mamednoor <mamed.gohabur@gmail.com>
2020-04-01 23:14:55 +0200

    some changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './style.css';
 
-function Contact(props) {
 
-
-return (
-<div className="Contact">
-    <img className="avatar" src={props.avatar} alt={props.name} />
-    <div>
-        <h4 className="name">{props.name}</h4>
-    <div className="status">
-        <span className={props.online ? 'status-online' : 'status-offline'}></span>
-        <p className="status-text">{props.online ? "Online" : "Offline"}</p>
-    </div>
-    </div>
-</div>
-);
+class Contact extends React.Component { 
+    constructor(props) {
+      super(props);
+      this.state = {
+        status : true,
+      };
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name}/>
+                <div>
+                    <h4 className="name">{this.props.name}</h4>
+                    <div className="status">
+                        <span className={this.props.status ? 'status-online' : 'status-offline'}></span>
+                        <button onClick={event => {const newStatus = !this.state.status; this.setState({status: newStatus});}}>{this.props.online ? 'online' : 'offline'}</button>
+                    </div>
+                </div>
+            </div>    
+        );
+    }
 }
-
-Contact.proTypes = {
-name: PropTypes.string.isRequired,
-avatar: PropTypes.string.isRequired,
-online: PropTypes.bool.isRequired,
-
-}
-
 
 export default Contact;
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -6,31 +6,26 @@ const users = [
     {
         name: "Robert Reyes",
         avatar: "https://i.skyrock.net/2318/19122318/pics/542003866_small.jpg",
-        online: false
     },
     
     {
         name: "Nellie Caldwell",
         avatar: "https://i.skyrock.net/1085/13431085/pics/345224551.jpg",
-        online: true
     },
     
     {
         name: "Vernon Mason",
         avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSor4mgdlMoNibiYxM5S3hbEiagOi3zD3kmoTUjiC3gLUihknCK&usqp=CAU",
-        online: true
     },
     
     {
         name: "Erica Hunt",
         avatar: "https://www.catizz.com/medias/common/chat-grosse-tete-mini.jpg",
-        online: false
     },
     
     {
         name: "Juanita Phillips",
-        avatar: "https://lh3.googleusercontent.com/proxy/F5mki-SjIwLBv1MVBoYCrSib_th9NskUBUXftcGxyHdtvDhlkkXz4N2-jTolk-HoBiRXuYBl5h1PoVXFNGExkJ6z90ve98X6G6v0nItjY_9CbQ",
-        online: true
+        avatar: "https://www.chakipet.com/wp-content/uploads/animaux-drole-tete-6-569x640.jpg",
     }
     ]
 
